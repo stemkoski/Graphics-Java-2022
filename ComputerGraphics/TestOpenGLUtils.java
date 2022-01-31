8280ff2fd1f60a775d7f02c5a7f7cff5489fbdb7
Lee Stemkoski <stemkoski@gmail.com>
2022-01-31 11:58:44 -0500

    adding TestOpenGL functions

--- ComputerGraphics/TestOpenGLUtils.java
+++ ComputerGraphics/TestOpenGLUtils.java
@@ -1,16 +1,57 @@
 import graphics.core.*;
 
+import static org.lwjgl.opengl.GL40.*;
+
 /**
  * Write a description of class TestOpenGLUtils here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class TestOpenGLUtils
+public class TestOpenGLUtils extends Base
 {
-    public static void main(String[] args)
+    int programRef;
+    // data array
+    int arrayRef;
+    public void initialize()
     {
+        /*
         String code = OpenGLUtils.readFile("shaders/center.vert");
         System.out.println(code);
+        
+        // test compiling shader
+        int shaderRef = OpenGLUtils.initializeShader(code, GL_VERTEX_SHADER);
+        System.out.println("Shader reference: " + shaderRef );
+        
+        String code2 = OpenGLUtils.readFile("shaders/yellow.frag");
+        int shaderRef2 = OpenGLUtils.initializeShader(code2, GL_FRAGMENT_SHADER);
+        System.out.println("Shader reference: " + shaderRef2 );
+        */
+        
+        programRef = OpenGLUtils.initializeProgram(
+             "shaders/center.vert", "shaders/yellow.frag");
+        
+        System.out.println("Program reference: " + programRef);
+        
+        arrayRef = glGenVertexArrays();
+        glBindVertexArray(arrayRef);
+    }
+    
+    public void update()
+    {
+        // declare GPU program to use
+        glUseProgram(programRef);
+        
+        glPointSize(20);
+        
+        // draw all the points
+        // parameters: 
+        //    draw style (points/lines/triangles), starting array index, # vertices
+        glDrawArrays(GL_POINTS, 0, 1);
+    }
+    
+    public static void main(String[] args)
+    {
+        new TestOpenGLUtils().run();
     }
 }
--- ComputerGraphics/graphics/core/OpenGLUtils.java
+++ ComputerGraphics/graphics/core/OpenGLUtils.java
@@ -1,6 +1,8 @@
 package graphics.core;
 
 import java.nio.file.*;
+// use method names without referring to class
+import static org.lwjgl.opengl.GL40.*;
 
 /**
  * methods to load, send, compile, link, check shader programs
@@ -10,6 +12,12 @@ import java.nio.file.*;
  */
 public class OpenGLUtils
 {
+   /**
+    * Load text file with shader code, and return a String with that code.
+    *
+    * @param fileName the shader file name
+    * @return a string containing file contents
+    */
    public static String readFile(String fileName)
    {      
        String text = "";
@@ -29,5 +37,77 @@ public class OpenGLUtils
        return text;
    }
 
-
+   /**
+    * Create a shader object on the GPU, send shader code to object, and compile.
+    *
+    * @param shaderCode contains all shader code as a string
+    * @param shaderType integer specifies vertex or fragment shader
+    * @return index representing shader program location on GPU
+    */
+   public static int initializeShader(String shaderCode, int shaderType)
+   {
+       // create a shader object on GPU
+       int shaderRef = glCreateShader(shaderType);
+       
+       // send source code to shader
+       glShaderSource(shaderRef, shaderCode);
+       
+       // compile shader code
+       glCompileShader(shaderRef);
+       
+       // store error codes
+       int[] status = new int[1];
+       
+       // check for errors; status is used as pass-by-reference
+       glGetShaderiv(shaderRef, GL_COMPILE_STATUS, status);
+       
+       if (status[0] == GL_FALSE)
+       {
+           // retrieve error message
+           String errorMessage = glGetShaderInfoLog(shaderRef);
+           
+           // quit java program, display error message
+           throw new RuntimeException(errorMessage);
+       }
+       
+       return shaderRef;
+   }
+   
+   public static int initializeProgram(
+      String vertexShaderFileName,
+      String fragmentShaderFileName )
+   {
+       // load shader code
+       String vertexShaderCode   = readFile(vertexShaderFileName);
+       String fragmentShaderCode = readFile(fragmentShaderFileName);
+       
+       // load and compile shaders
+       int vertexShaderRef = 
+               initializeShader(vertexShaderCode, GL_VERTEX_SHADER);
+       int fragmentShaderRef = 
+               initializeShader(fragmentShaderCode, GL_FRAGMENT_SHADER);
+               
+       // create GPU program object to store shaders
+       int programRef = glCreateProgram();
+       
+       // put shaders into program object
+       glAttachShader(programRef, vertexShaderRef);
+       glAttachShader(programRef, fragmentShaderRef);
+       
+       // link shader programs, so that data from vertex shader
+       //   is sent to the fragment shader
+       glLinkProgram(programRef);
+       
+       // TODO: error checking
+       
+       // success~
+       return programRef;
+   }
+   
+   
+   
+   
+   
+   
+   
 }
