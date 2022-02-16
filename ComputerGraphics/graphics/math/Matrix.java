cfa08c2e0ca087dab90bef20ebe4d1ee08dd9684
Lee Stemkoski <stemkoski@gmail.com>
2022-02-16 11:51:13 -0500

    start Vector class

---
+++ ComputerGraphics/graphics/math/Matrix.java
@@ -0,0 +1,35 @@
+package graphics.math;
+
+
+/**
+ * Write a description of class Matrix here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Matrix
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Matrix
+     */
+    public Matrix()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
